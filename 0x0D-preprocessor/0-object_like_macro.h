26cd651d0d410abe9b5f936559f34eedf442b2eb
Abdi <blackabdime@gmail.com>
2022-08-01 14:32:42 -0700

    res, typedef

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,14 +1,4 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-
-/*
- * File: 0-object_like_macro.h
- * Auth: Gedeon Obae Gekonge
- * Desc: Header file that defines a macro named
- *       SIZE as an abbreviation for the token 1024.
- */
-
-
+#ifndef _HEADER_
+#define _HEADER_
 #define SIZE 1024
-
 #endif
